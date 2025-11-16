13ebcedda10273edf30e6d996d65a2710260ab6c
Johny Tarbouch <johnytarbouch@gmail.com>
2025-11-16 16:17:04 +0100

    Change From cartpole to go2 env

--- isaac_sim/robotics_project_25/source/robotics_project_25/robotics_project_25/tasks/direct/robotics_project_25/robotics_project_25_env_cfg.py
+++ isaac_sim/robotics_project_25/source/robotics_project_25/robotics_project_25/tasks/direct/robotics_project_25/robotics_project_25_env_cfg.py
@@ -1,48 +1,51 @@
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
-# All rights reserved.
-#
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers
 # SPDX-License-Identifier: BSD-3-Clause
 
-from isaaclab_assets.robots.cartpole import CARTPOLE_CFG
-
-from isaaclab.assets import ArticulationCfg
 from isaaclab.envs import DirectRLEnvCfg
 from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.sim import SimulationCfg
 from isaaclab.utils import configclass
+from isaaclab.assets import ArticulationCfg
+from isaaclab_assets.robots.unitree import UNITREE_GO2_CFG
 
 
 @configclass
 class RoboticsProject25EnvCfg(DirectRLEnvCfg):
-    # env
+    # env settings
     decimation = 2
-    episode_length_s = 5.0
-    # - spaces definition
-    action_space = 1
-    observation_space = 4
+    episode_length_s = 10.0
+
+    # RL interface
+    action_space = 12
+    observation_space = 48
     state_space = 0
 
     # simulation
-    sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
+    sim: SimulationCfg = SimulationCfg(
+        dt=1 / 120,
+        render_interval=decimation,
+    )
 
-    # robot(s)
-    robot_cfg: ArticulationCfg = CARTPOLE_CFG.replace(prim_path="/World/envs/env_.*/Robot")
+    # robot configuration
+    robot_cfg: ArticulationCfg = UNITREE_GO2_CFG.replace(
+        prim_path="/World/envs/env_.*/Go2",
+        init_state=ArticulationCfg.InitialStateCfg(
+            pos=(0.0, 0.0, 0.4),
+            rot=(1.0, 0.0, 0.0, 0.0),
+            joint_pos={
+                "FL_hip_joint": 0.0, "FR_hip_joint": 0.0,
+                "RL_hip_joint": 0.0, "RR_hip_joint": 0.0,
+                "FL_thigh_joint": 0.9, "FR_thigh_joint": 0.9,
+                "RL_thigh_joint": 0.9, "RR_thigh_joint": 0.9,
+                "FL_calf_joint": -1.8, "FR_calf_joint": -1.8,
+                "RL_calf_joint": -1.8, "RR_calf_joint": -1.8,
+            },
+        ),
+    )
 
-    # scene
-    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=4096, env_spacing=4.0, replicate_physics=True)
-
-    # custom parameters/scales
-    # - controllable joint
-    cart_dof_name = "slider_to_cart"
-    pole_dof_name = "cart_to_pole"
-    # - action scale
-    action_scale = 100.0  # [N]
-    # - reward scales
-    rew_scale_alive = 1.0
-    rew_scale_terminated = -2.0
-    rew_scale_pole_pos = -1.0
-    rew_scale_cart_vel = -0.01
-    rew_scale_pole_vel = -0.005
-    # - reset states/conditions
-    initial_pole_angle_range = [-0.25, 0.25]  # pole angle sample range on reset [rad]
-    max_cart_pos = 3.0  # reset if cart exceeds this position [m]
+    # Scene
+    scene: InteractiveSceneCfg = InteractiveSceneCfg(
+        num_envs=1,
+        env_spacing=4.0,
+        replicate_physics=True,
+    )
