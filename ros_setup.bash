4816002f3730a2a2664dd03da355d5f04c663834
Johny Tarbouch <johnytarbouch@gmail.com>
2025-12-11 14:15:50 +0100

    scripts

--- ros_setup.bash
+++ ros_setup.bash
@@ -1,3 +1,4 @@
+echo "Remember to **source** me!"
 source /opt/ros/humble/setup.bash
 source ./deps/rclpy/IsaacSim-ros_workspaces/build_ws/humble/humble_ws/install/local_setup.bash
 conda activate env_isaaclab3
